70c33adabbb9772766034c9f8d810d4a8b7c6653
abaudart <baudart.alexandre@etud.univ-pau.fr>
2026-09-23 21:41:32 -0400

    Update gitignore

--- heart_disease_prediction/data/data_exploration.py
+++ heart_disease_prediction/data/data_exploration.py
@@ -15,12 +15,13 @@ if __name__ == "__main__" :
         },
         show_df_info=False
     )
-    # dataset.split_data_csv(train_size=0.8, calib_size=0.1, target="target_binary")
+    dataset.split_data_csv(train_size=0.8, calib_size=0.1, target="target_binary")
 
     df = dataset.get_original_df(binarize=True)
 
-    check_missing_values(df)
+    # check_missing_values(df)
     # check_count(df, target="target_binary")
 
     # heatmap_correlation(df, dropped_cols=["id", "dataset"] + [c for c in df.columns if "missing" in c])
+    # heatmap_correlation(df, dropped_cols=["id", "dataset", "target_binary"] + [c for c in df.columns if "missing" in c])
 
